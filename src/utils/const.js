2a4ccf1e12e6d696e111596dcdd23b0902a6989f
cashplk <cash.zhao@gmail.com>
2021-02-23 09:20:15 +0000

    修改mapbox Token。

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoiY2FzaHBsayIsImEiOiJjaWg3bWp6dHcwdDRudXVraWt0dXR6dGl5In0.Y82Xmh0NIm2UdDurTuoRDQ';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
