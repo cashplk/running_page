17264b5367a2f0597abefb269c2fc7fbb4b7c8cb
cashplk <cash.zhao@gmail.com>
2022-06-09 17:59:16 +0000

    还原原始配置文件。

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  pathPrefix: `/`, // Change to `/running_page` when running on github pages
+  pathPrefix: '/', // Change to `/running_page` when running on github pages
   siteMetadata: {
-    siteTitle: 'Workouts Map',
-    siteUrl: 'https://cashplk.com',
-    logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+    siteTitle: 'Running Page',
+    siteUrl: 'https://yihong.run',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://cashplk.com',
+        url: 'https://yihong.run/running',
       },
       {
         name: 'About',
-        url: 'https://github.com/cashplk/running_page',
+        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
       },
     ],
   },
@@ -57,7 +57,12 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-sass',
       options: {
-        precision: 8,
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
       },
     },
     {
